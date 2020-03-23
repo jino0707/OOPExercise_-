4ef237d6cc058db2a9f10cb9c8c83db57ec64d89
jino0 <jino0@DESKTOP-Q2CQ5CT>
2020-03-23 22:09:04 +0900

    깃 허브 테스트 1

--- src/member/model/vo/Member/Member.java
+++ src/member/model/vo/Member/Member.java
@@ -1,7 +1,7 @@
 package member.model.vo.Member;
 
 public class Member {
-
+	
 	
 	
 }
---
+++ src/member/run/Helloworld.java
@@ -0,0 +1,9 @@
+package member.run;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
